f3263dec8b3f0adf9469d6608c9c8e7e078b4572
Fei Chen <boxchenfei@gmail.com>
2023-03-04 20:50:12 +0100

    Remove React.Strict, otherwise the UseEffect will be triggered twice.

--- src/index.js
+++ src/index.js
@@ -9,11 +9,7 @@ import {
 } from './dummyDataForTest';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <VPPanel nodesConfig={initialNodes} edgesConfig={initialEdges} />
-  </React.StrictMode>
-);
+root.render(<VPPanel nodesConfig={initialNodes} edgesConfig={initialEdges} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
